af7e19144e044287b8f124190ae45d5f18bcde57
jacobmammoliti <jacob@homepage.ca>
2023-03-05 17:13:44 -0500

    upgrades firewall module

--- 0-bootstrap/networking.tf
+++ 0-bootstrap/networking.tf
@@ -14,54 +14,20 @@ module "vpc-shared" {
 }
 
 module "vpc-shared-firewall" {
-  source = "github.com/GoogleCloudPlatform/cloud-foundation-fabric.git//modules/net-vpc-firewall?ref=v18.0.0"
+  source = "github.com/GoogleCloudPlatform/cloud-foundation-fabric.git//modules/net-vpc-firewall?ref=v20.0.0"
 
   project_id = module.project.project_id
   network    = module.vpc-shared.name
 
-  custom_rules = {
-    fw-allow-all-internal = {
-      description          = "Allow internal traffic on the default network"
-      direction            = "INGRESS"
-      action               = "allow"
-      sources              = []
-      ranges               = ["rfc1918"] # 172.16.0.0/12, 10.0.0.0/8, 192.168.0.0/16
-      targets              = []
-      use_service_accounts = false
-      rules = [
-        { protocol = "tcp", ports = ["0-65535"] },
-        { protocol = "udp", ports = ["0-65535"] },
-        { protocol = "icmp", ports = [] }
+  ingress_rules = {
+    allow-ingress-rfc1918 = {
+      description = "Allow internal traffic on the default network"
+      source_ranges = [
+        "172.16.0.0/12", "10.0.0.0/8", "192.168.0.0/16"
       ]
-      extra_attributes = {}
+      sources = []
+      targets = []
     }
-    fw-allow-ssh-external = {
-      description          = "Allow external SSH traffic on the default network"
-      direction            = "INGRESS"
-      action               = "allow"
-      sources              = []
-      ranges               = ["34.0.0.0/8", "35.0.0.0/8"] # GCP's IP block allowing SSH only on Cloud Shell
-      targets              = []
-      use_service_accounts = false
-      rules = [
-        { protocol = "tcp", ports = ["22"] }
-      ]
-      extra_attributes = {}
-    }
-    # fw-allow-http-https-external = {
-    #   description          = "Allow external HTTP/HTTPS traffic on the default network"
-    #   direction            = "INGRESS"
-    #   action               = "allow"
-    #   sources              = []
-    #   ranges               = ["0.0.0.0/0"]
-    #   targets              = []
-    #   use_service_accounts = false
-    #   rules = [
-    #     { protocol = "tcp", ports = ["80"] },
-    #     { protocol = "tcp", ports = ["443"] },
-    #   ]
-    #   extra_attributes = {}
-    # }
   }
 }
 
